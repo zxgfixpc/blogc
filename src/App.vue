30162c07f7bfff1498177b685d36153e61aec4cc
zhaoxuguang <neucontrolzxg@163.com>
2024-06-23 21:25:22 +0800

    导航栏开发

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,29 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <router-view /> 
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<!-- <script setup lang="ts">
+import HomePage from '@/pages/HomePage.vue'
+</script> -->
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style>
+@import './style/common.scss';
 
-nav {
-  width: 100%;
-  font-size: 12px;
+#app {
+  font-family: Arial, Helvetica, sans-serif;
   text-align: center;
-  margin-top: 2rem;
-}
+  color: #eddede;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+  /* 设置背景色为白色 */  
+  background-color: white;  
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+   /* 占据整个视口 */  
+  position: fixed;  
+  top: 0;  
+  left: 0;  
+  right: 0;  
+  bottom: 0; 
 }
 </style>
---
+++ src/pages/home/HomePage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="homePage">
+      <el-container class="homeContainer">
+        <el-header class="homeHeader">
+          <!-- 导航栏 -->
+          <Navigation />
+        </el-header>
+        <el-container>
+          <el-aside class="homeAside"></el-aside>
+
+          <el-main class="homeMain">
+            <!-- 主展示区 -->
+            
+          </el-main>
+
+          <el-aside class="homeAside"></el-aside>
+        </el-container>
+      </el-container>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import Navigation from './components/Navigation.vue';
+import Edit from './components/Edit.vue';
+import Recommend from './components/Recommend.vue';
+</script>
+
+<style scoped lang="scss">
+.homeContainer{
+
+    .homeHeader{
+        border-bottom: 1px solid rgb(223, 219, 219);
+        background: white;
+        height:60px;
+    }
+    .homeAside{
+        background: white;
+        flex: 1;
+        height: 100vh;
+    }
+    .homeMain{
+        flex: 0 1 70vw;
+        min-width: 100px;
+        height: 100vh;
+    }
+}
+
+</style>
