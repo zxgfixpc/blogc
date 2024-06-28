e79325f1b737e78536500df38fbefb5247a1b9ab
zhaoxuguang <neucontrolzxg@163.com>
2024-06-28 21:49:03 +0800

    增加列表+markdown

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,19 @@ import router from './router/router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia' 
+import mavonEditor from 'mavon-editor'  //引入mavon-editor 就是上面所安装的
+import 'mavon-editor/dist/css/index.css';
+
+import VueMarkdownEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+
+import Prism from 'prismjs';
+
+VueMarkdownEditor.use(vuepressTheme, {
+  Prism,
+});
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -11,5 +24,7 @@ const pinia = createPinia()
 app.use(router)
 app.use(ElementPlus)
 app.use(pinia)
+app.use(mavonEditor) 
+app.use(VueMarkdownEditor)
 
 app.mount('#app')
--- src/pages/home/subs/Edit.vue
+++ src/pages/home/subs/Edit.vue
@@ -1,4 +1,16 @@
 <template> 
-  编辑 1
-  编辑 2
+  <p> 编辑 </p>
+  <v-md-editor v-model="article.text" height="400px"></v-md-editor>
+  <el-button @click="sendBlog">发送</el-button>
 </template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+const article = reactive({
+	text:''
+})
+
+function sendBlog() {
+  console.log(article.text)
+}
+</script>
